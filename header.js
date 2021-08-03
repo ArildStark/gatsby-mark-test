af0c7dd58ce625ca711af315c2d73413f996b93c
Arild Stark <arild.payag@amici.com.ph>
2021-08-03 15:40:16 +0800

    My First File

---
+++ header.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const header = () =>  {
+
+return(
+
+    <navbar>
+    <Link to="/">Home</Link>
+    <Link to="/about/" >About</Link>
+    <Link to="/blogs/" >Blog</Link>
+    <Link to="/contact/" >Contact</Link>
+        </navbar> 
+
+)
+
+}
+
+export default Blogs
---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React from "react"
+import NavHeader from "../components/header"
+import Divtextheader from "../components/divheadertext"
+
+
+const about = () =>  {
+
+return(
+<div>
+<Divtextheader>
+</Divtextheader>
+<NavHeader>
+</NavHeader>
+<img class="avenger-hero-img" src="hero-img.jpg"  ></img>
+
+<h1>About Charlene</h1>
+
+
+</div>
+)
+
+}
+
+export default about
